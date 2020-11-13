147668d485335655e4d21d00433be97708ae5bb9
Anthony Li <me@anli.dev>
2020-11-13 16:41:22 -0500

    Initial Commit

---
+++ Nyoom/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Nyoom
+//
+//  Created by Anthony Li on 11/13/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
